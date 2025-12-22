384bbee441440740e8b4986f55e29ba2ee6f80ab
SSM <blussm@tsis.co.kr>
2025-12-22 10:12:16 +0900

    initial Commit

---
+++ src/main/java/com/rentify/rentify_api/RentifyApiApplication.java
@@ -0,0 +1,13 @@
+package com.rentify.rentify_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentifyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rentify/rentify_api/RentifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentify.rentify_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
