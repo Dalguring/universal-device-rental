d949d5c7a9623762b594cb3cfe2c318b37c3194f
Daljee <blussm@kakao.com>
2026-01-04 18:40:05 +0900

    perf: swagger 내 localhost 서버 추가 - api 호출 테스트를 위해 localhost 서버 추가

--- src/main/java/com/rentify/rentify_api/common/config/SwaggerConfig.java
+++ src/main/java/com/rentify/rentify_api/common/config/SwaggerConfig.java
@@ -13,15 +13,19 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        Server server = new Server();
-        server.setUrl("http://localhost:8080");
-        server.setDescription("back-end-server");
+        Server devServer = new Server();
+        devServer.setUrl("http://43.201.87.180:8080");
+        devServer.setDescription("back-end-server");
+
+        Server localServer = new Server();
+        localServer.setUrl("http://localhost:8080");
+        localServer.setDescription("local-server");
 
         return new OpenAPI()
             .info(new Info()
                 .title("Universal-device-rental")
                 .version("v1")
                 .description("다방면용 전자기기 대여 서비스 백엔드 서버")
-            ).servers(List.of(server));
+            ).servers(List.of(devServer, localServer));
     }
 }
