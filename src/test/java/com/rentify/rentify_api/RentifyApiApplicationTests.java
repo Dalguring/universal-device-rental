5a8c5bd3c5ae6e0535f7fbfaf7692fd4bdf27a7f
Lee jae hyeong <ftko6252@naver.com>
2026-01-08 15:33:46 +0900

    fix : test jwt secret

--- src/test/java/com/rentify/rentify_api/RentifyApiApplicationTests.java
+++ src/test/java/com/rentify/rentify_api/RentifyApiApplicationTests.java
@@ -3,7 +3,7 @@ package com.rentify.rentify_api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "jwt.secret=test-secret")
 class RentifyApiApplicationTests {
 
 	@Test
