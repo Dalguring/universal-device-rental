56c2ebb73ad476c65a0a8b47f5fbe03a24ac2619
SSM <blussm@tsis.co.kr>
2026-01-23 15:40:56 +0900

    feature: 카테고리 조회 api 개발

--- src/main/java/com/rentify/rentify_api/category/controller/CategoryController.java
+++ src/main/java/com/rentify/rentify_api/category/controller/CategoryController.java
@@ -1,10 +1,27 @@
 package com.rentify.rentify_api.category.controller;
 
+import com.rentify.rentify_api.category.dto.CategoryResponse;
+import com.rentify.rentify_api.category.dto.CategoryInfo;
+import com.rentify.rentify_api.category.service.CategoryService;
+import com.rentify.rentify_api.common.response.ApiResponse;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/categories")
-public class CategoryController {
+public class CategoryController implements CategoryApiDocs {
+    private final CategoryService categoryService;
 
+    @Override
+    @GetMapping
+    // TODO: cache 처리
+    public ResponseEntity<ApiResponse<CategoryResponse>> getCategory() {
+        List<CategoryInfo> categoryInfos = categoryService.getCategory();
+        return ResponseEntity.ok(ApiResponse.success(new CategoryResponse(categoryInfos)));
+    }
 }
---
+++ src/main/java/com/rentify/rentify_api/category/dto/CategoryResponse.java
@@ -0,0 +1,7 @@
+package com.rentify.rentify_api.category.dto;
+
+import java.util.List;
+
+public record CategoryResponse(List<CategoryInfo> categories) {
+
+}
