c71c817dcb190aedc8f6c808ba448e0858b90f95
Daljee <blussm@kakao.com>
2026-03-30 11:16:34 +0900

    fix: redis 설정 변경

--- src/main/java/com/rentify/rentify_api/common/config/RedisConfig.java
+++ src/main/java/com/rentify/rentify_api/common/config/RedisConfig.java
@@ -8,8 +8,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import tools.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import tools.jackson.databind.jsontype.PolymorphicTypeValidator;
 
 @Configuration
 @EnableCaching
@@ -18,15 +21,23 @@ public class RedisConfig {
 
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
-        JacksonJsonRedisSerializer<Object> serializer =
-            new JacksonJsonRedisSerializer<>(Object.class);
+        PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder()
+            .allowIfBaseType("com.rentify")
+            .allowIfBaseType("java")
+            .build();
+
+        GenericJacksonJsonRedisSerializer serializer = GenericJacksonJsonRedisSerializer
+            .builder()
+            .enableDefaultTyping(ptv)
+            .build();
 
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
             .entryTtl(Duration.ofMinutes(10))
-            .serializeValuesWith(
-                RedisSerializationContext.SerializationPair
-                    .fromSerializer(serializer)
-            );
+            .serializeKeysWith(RedisSerializationContext.SerializationPair
+                .fromSerializer(new StringRedisSerializer()))
+            .serializeValuesWith(RedisSerializationContext.SerializationPair
+                .fromSerializer(serializer))
+            .disableCachingNullValues();
 
         return RedisCacheManager.builder(connectionFactory)
             .cacheDefaults(config)
